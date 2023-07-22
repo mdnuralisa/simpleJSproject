24f4beb6b11d63e9b1e6bfabfe37c9a20dc0ab44
Lisa <md.nuralisa@gmail.com>
2023-07-23 01:26:30 +0800

    change function name

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function paint(color) {
 //     const randomColor = Math.floor(Math.random()*16777215).toString(16);
 //     randomColor.style = `background-color:${"#" + randomColor}`;
 // }
-function randomcolor(){
+function randomized(){
     const circle = document.getElementById('circleID');
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     circle.style = `background-color:${"#" + randomColor}`;
